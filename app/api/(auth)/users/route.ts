8c1f493b8031575d7ae917bf898983f47019d928
silvante <khamidov.ko@gmail.com>
2024-09-25 01:07:26 +0300

    fix

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -2,8 +2,28 @@ import connect from "@/lib/db";
 import User from "@/lib/models/user";
 import { NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import nodemailer from "nodemailer";
 
 const cyfer = bcryptjs.genSaltSync(8);
+
+// // transporter for mail
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   host: "smtp.gmail.com",
+//   auth: {
+//     user: process.env.AUTH_EMAIL,
+//     pass: process.env.AUTH_PASSWORD,
+//   },
+// });
+
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("ready for OTP verifications");
+//     console.log(success);
+//   }
+// });
 
 export const GET = async () => {
   try {
